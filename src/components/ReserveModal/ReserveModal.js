7f12f4c8a3302e23b23b346ff7b96fceeb701618
Carlos B <kalbulnes@gmail.com>
2026-05-23 21:44:43 -0500

    importar useEffect el ReserveModal

--- src/components/ReserveModal/ReserveModal.js
+++ src/components/ReserveModal/ReserveModal.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./ReserveModal.module.css";
 
 function ReserveModal({ gift, onClose, onReserve }) {
